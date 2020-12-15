4e2090b994da68537ed7fc2aefaec62effa7ec16
darylsong <darylsong@tencent.com>
2020-12-15 14:56:10 +0800

    提交

--- serverjson.js
+++ serverjson.js
@@ -4,6 +4,9 @@ var fs = require("fs");
 var path = require("path");
 var url = "https://www.meishij.net/chufang/diy/jiangchangcaipu/?&page=";
 var start = 1;
+const res = [];
+
+//开始爬取页面数据
 const sleep = numberMillis => {
   const exitTime = +new Date() + numberMillis;
   while (true) {
@@ -19,19 +22,30 @@ const getMovies = async (url, start) => {
   };
   let $ = await rp(opts);
   $("#listtyle1_list > div").each((index, ele) => {
-    let src = $(ele).find("img").attr("src");
-    let name = start + "_" + $(ele).find("a").attr("title") + ".jpg";
-    rp(src).pipe(fs.createWriteStream(path.resolve(__dirname + "/img", name)));
+    res.push({
+      title: $(ele).find("a").attr("title"),
+      img: $(ele).find("img").attr("src"),
+      desc: $(ele).find(".li2").text(),
+      type: $(ele).find(".li1").text(),
+    });
   });
-
   if (start < 56) {
     console.log(`爬取完成第${start}页,用时${+new Date() - startTime}`);
     sleep(50);
     getMovies(url, start + 1);
   } else {
     console.log(`爬取完成`);
+    var writerStream = fs.createWriteStream("output.txt");
+    writerStream.write(JSON.stringify(res));
+    writerStream.end();
+    writerStream.on("finish", function () {
+      console.log("写入完成。");
+    });
+
+    writerStream.on("error", function (err) {
+      console.log(err.stack);
+    });
   }
 };
 
-//开始爬取页面数据
 getMovies(url, start);
