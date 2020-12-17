f436d5e00492f8a6b2268c18468b4d65aab73939
darylsong <darylsong@tencent.com>
2020-12-17 16:57:41 +0800

    时光网

--- server.js
+++ server.js
@@ -1,40 +1,49 @@
 const rp = require("request-promise");
-const cheerio = require("cheerio");
-const url = "https://movie.douban.com";
-const debug = require("debug")("movie:read");
-const read = async url => {
-  debug("开始读取最近上映的电影");
-  const opts = {
-    url,
-    transform: body => cheerio.load(body),
-  };
-  let $ = await rp(opts);
-  let result = []; // 结果数组
-  $("#screening li.ui-slide-item").each((index, item) => {
-    let ele = $(item);
-    let name = ele.data("title");
-    let score = ele.data("rate") || "暂无评分";
-    let href = ele.find(".poster a").attr("href");
-    let image = ele.find("img").attr("src");
-    let id = href && href.match(/(\d+)/)[1];
-    image = image && image.replace(/jpg$/, "webp");
-
-    if (!name || !image || !href) return;
-
-    result.push({
-      name,
-      score,
-      href,
-      image,
-      id,
+sconst config = require("./config");
+const fs = require("fs");
+const path = require("path");
+const page = 1;
+const errors = [];
+const success = [];
+const obj = url => ({
+  url,
+  headers: {
+    "User-Agent": config.randomHead(),
+    "X-Forwarded-For": config.returnIp(),
+  },
+});
+const read = async page => {
+  const time = +new Date();
+  const url = `http://video.mtime.com/api/videoSearch/getFilterData?h=movie&y=%E5%85%A8%E9%83%A8%E5%B9%B4%E4%BB%A3&p=1&s=2&i=${page}&c=30`;
+  const { movieIntegrateList } = JSON.parse(await rp(obj(url)));
+  movieIntegrateList.map(({ coverPath: src, titleCn: na }) => {
+    const name = na + ".jpg";
+    const out = fs.createWriteStream(path.resolve(__dirname + "/imgs", name));
+    rp(obj(`http:${src}`)).pipe(out);
+    out.on("error", err => {
+      errors.push(err);
+      console.log(`丢失${errors.length}张图片 原因${err}`);
     });
-    debug(`正在读取电影：${name}`);
+    out.on("finsh", () => {
+      success.push(name);
+      console.log(`下载成功--------------：${name}`);
+    });
+    out.on("ok", () => {
+      success.push(name);
+      console.log(`下载成功--------------：${name}`);
+    });
   });
-  return result;
+  if (page < 30) {
+    console.log(`爬取----${page}----页 用时--${+new Date() - time} `);
+    read(page + 1);
+  } else {
+    // var writerStream = fs.createWriteStream("output.json");
+    // writerStream.write(JSON.stringify(p));
+    // writerStream.end();
+    // writerStream.on("finish", function () {
+    //   console.log("写入完成。");
+    // });
+    console.log("成功，共下载" + success.length + "张");
+  }
 };
-
-const start = async url => {
-  let res = await read(url);
-  console.log(res);
-};
-start(url);
+read(page);
