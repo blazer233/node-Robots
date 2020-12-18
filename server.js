a7d62c68cc90b4458a88a7eb4516101f9c15996c
darylsong <darylsong@tencent.com>
2020-12-18 14:06:15 +0800

    add

--- server.js
+++ server.js
@@ -1,37 +1,67 @@
 const rp = require("request-promise");
 const cheerio = require("cheerio");
+const config = require("./config");
 var fs = require("fs");
 var path = require("path");
-var url = "https://www.meishij.net/chufang/diy/jiangchangcaipu/?&page=";
+const errors = [];
+const success = [];
+const photo = [];
 var start = 1;
-const sleep = numberMillis => {
-  const exitTime = +new Date() + numberMillis;
-  while (true) {
-    if (+new Date() > exitTime) return;
-  }
-};
-const getMovies = async (url, start) => {
+// const sleep = numberMillis => {
+//   const exitTime = +new Date() + numberMillis;
+//   while (true) {
+//     if (+new Date() > exitTime) return;
+//   }
+// };
+const opts = ({ url, transform }) => ({
+  "X-Forwarded-For": config.returnIp(),
+  "User-Agent": config.randomHead(),
+  transform,
+  url,
+});
+const getMovies = async start => {
   console.log(`开始爬取第${start}页`);
+  //https://www.freeimages.com/cn/image/${start}
+  const url = `https://www.istockphoto.com/photos/girl?mediatype=photography&page=${start}&phrase=girl&sort=best`;
   const startTime = +new Date();
-  const opts = {
-    url: `${url}${start}`,
-    transform: body => cheerio.load(body),
-  };
-  let $ = await rp(opts);
-  $("#listtyle1_list > div").each((index, ele) => {
-    let src = $(ele).find("img").attr("src");
-    let name = start + "_" + $(ele).find("a").attr("title") + ".jpg";
-    rp(src).pipe(fs.createWriteStream(path.resolve(__dirname + "/img", name)));
+  let $ = await rp(opts({ url, transform: body => cheerio.load(body) }));
+  $("li.item").each(async (index, ele) => {
+    let src = $(ele).find("a").attr("href");
+    let name = start + "_" + $(ele).find("a").attr("title") + "_.jpg";
+    let $_ = await rp(
+      opts({
+        url: `https://www.freeimages.com${src}`,
+        transform: body => cheerio.load(body),
+      })
+    );
+    photo.push({ url: $_(".preview img").attr("src"), name });
   });
 
-  if (start < 56) {
+  if (start < 10) {
     console.log(`爬取完成第${start}页,用时${+new Date() - startTime}`);
-    sleep(50);
-    getMovies(url, start + 1);
+    getMovies(start + 1);
   } else {
-    console.log(`爬取完成`);
+    let successPhoto = photo.filter(({ url }) => url);
+    let errlen = photo.length - successPhoto.length;
+    console.log(
+      `一共${photo.length}张，成功${successPhoto.length}张,失败${errlen}张`
+    );
+    console.log(`开始下载`);
+    successPhoto.map(({ url, name }) => {
+      let out = fs.createWriteStream(path.resolve(__dirname + "/img", name));
+      rp(opts({ url })).pipe(out);
+      out.on("error", err => {
+        errors.push(0);
+        console.log(`错误原因：${err} 共出错${errors.length}张`);
+      });
+      out.on("finsh", err => {
+        success.push(1);
+        console.log(`下载成功`);
+      });
+    });
+    console.log(`下载完成 一共成功${success.length}张，失败${errors.length}张`);
   }
 };
 
 //开始爬取页面数据
-getMovies(url, start);
+getMovies(start);
--- serverjson.js
+++ serverjson.js
@@ -1,51 +1,50 @@
 const rp = require("request-promise");
 const cheerio = require("cheerio");
+const config = require("./config");
 var fs = require("fs");
 var path = require("path");
-var url = "https://www.meishij.net/chufang/diy/jiangchangcaipu/?&page=";
+const errors = [];
+const success = [];
+const photo = [];
 var start = 1;
-const res = [];
-
-//开始爬取页面数据
-const sleep = numberMillis => {
-  const exitTime = +new Date() + numberMillis;
-  while (true) {
-    if (+new Date() > exitTime) return;
-  }
-};
-const getMovies = async (url, start) => {
+const opts = ({ url, transform }) => ({
+  "X-Forwarded-For": config.returnIp(),
+  "User-Agent": config.randomHead(),
+  transform,
+  url,
+});
+const getMovies = async start => {
   console.log(`开始爬取第${start}页`);
+  const url = `http://sy.cndesign.com/list_${start}.html`;
   const startTime = +new Date();
-  const opts = {
-    url: `${url}${start}`,
-    transform: body => cheerio.load(body),
-  };
-  let $ = await rp(opts);
-  $("#listtyle1_list > div").each((index, ele) => {
-    res.push({
-      title: $(ele).find("a").attr("title"),
-      img: $(ele).find("img").attr("src"),
-      desc: $(ele).find(".li2").text(),
-      type: $(ele).find(".li1").text(),
+  let $ = await rp(opts({ url, transform: body => cheerio.load(body) }));
+  $(".c-l_ul.clear>li").each(async (index, ele) => {
+    let src = $(ele).find(".pl_img_box a").attr("href");
+    let _$ = await rp(
+      opts({
+        url: src,
+        transform: body => cheerio.load(body),
+      })
+    );
+    _$(".crop_img img").each((_index, el) => {
+      let _url = _$(el).attr("src") && _$(el).attr("src");
+      let name = index + _$(el).attr("title") + _index + ".jpeg";
+      let out = fs.createWriteStream(path.resolve(__dirname + "/imgs", name));
+      rp(opts({ url: _url })).pipe(out);
+      out.on("error", err => {
+        errors.push(0);
+        console.log(`错误原因：${err} 共出错${errors.length}张`);
+      });
     });
   });
-  if (start < 56) {
-    console.log(`爬取完成第${start}页,用时${+new Date() - startTime}`);
-    sleep(50);
-    getMovies(url, start + 1);
-  } else {
-    console.log(`爬取完成`);
-    var writerStream = fs.createWriteStream("output.txt");
-    writerStream.write(JSON.stringify(res));
-    writerStream.end();
-    writerStream.on("finish", function () {
-      console.log("写入完成。");
-    });
 
-    writerStream.on("error", function (err) {
-      console.log(err.stack);
-    });
+  if (start < 20) {
+    console.log(`爬取完成第${start}页,用时${+new Date() - startTime}`);
+    getMovies(start + 1);
+  } else {
+    console.log("完成");
   }
 };
 
-getMovies(url, start);
+//开始爬取页面数据
+getMovies(start);
