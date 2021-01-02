34cbb75f406ab5333d162cd6807b62c8caf39ffe
Song <52182701+blazer233@users.noreply.github.com>
2021-01-02 18:15:35 +0800

    爬取所有图片

--- puppeteer/puppeteerPactice.js
+++ puppeteer/puppeteerPactice.js
@@ -11,12 +11,14 @@
  */
 const rp = require("request-promise");
 const fs = require("fs");
-const { sleep, returnIp, randomHead } = require("../config");
+const { Sleep, returnIp, randomHead } = require("../config");
 const puppeteer = require("puppeteer");
 const path = require("path");
 var msg = [];
 var error = [];
 var result = [];
+var all = [];
+var next;
 const opts = ({ url }) => ({
   "X-Forwarded-For": returnIp(),
   "User-Agent": randomHead(),
@@ -25,27 +27,46 @@ const opts = ({ url }) => ({
 const down = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("http://desk.zol.com.cn/meinv");
-  await page.waitForSelector(".pic", { visible: true }); //等待id为login_field的元素出现继续执行
-  const arr = await page.$$eval(".pic", el =>
-    el.map((i, index) =>
-      i.getAttribute("href").includes("http")
-        ? { href: i.getAttribute("href"), index }
-        : { href: "http://desk.zol.com.cn" + i.getAttribute("href"), index }
-    )
-  );
-  console.log(`----当前页面有${arr.length}组----${new Date()}`);
-  for (let i = 0; i < arr.length; i++) {
-    console.log(
-      `-正在爬取第${i + 1}组--${new Date()}--下载地址：${arr[i].href}`
+  await page.goto("http://desk.zol.com.cn/fengjing/");
+  await page.waitForSelector(".photo-list-padding .pic", { visible: true }); //等待id为login_field的元素出现继续执行
+  await page.waitForSelector("#pageNext", { visible: true }); //等待id为login_field的元素出现继续执行
+  next = await page.$$eval("#pageNext", e => e.length);
+  while (next) {
+    Sleep(1000);
+    const arr = await page.$$eval(".photo-list-padding .pic", el =>
+      el.map((i, index) =>
+        i.getAttribute("href").includes("http")
+          ? { href: i.getAttribute("href"), index }
+          : { href: "http://desk.zol.com.cn" + i.getAttribute("href"), index }
+      )
     );
-    await page.goto(arr[i].href);
+    all = [...all, ...arr];
+    const href_ = await page.$eval("#pageNext", e => e.href);
+    const index_ = await page.$eval(".page .active", e => e.textContent);
+    await page.goto(href_);
+    next = await page.$$eval("#pageNext", e => e.length);
+    console.log(
+      `当前第${index_}页，照片总集数为${
+        all.length
+      }组----当前时间为 ${new Date()}`
+    );
+  }
+
+  for (let i = 0; i < all.length; i++) {
+    Sleep(1000);
+    console.log(
+      `-正在爬取第${i + 1}组--下载地址：${all[i].href}--${new Date()}`
+    );
+    await page.goto(all[i].href);
     const imgs = await page.$$eval("#showImg li a", el =>
       el.map(i => i.getAttribute("href"))
     );
     msg = [...msg, ...imgs];
   }
-  console.log(`----所有${msg.length}组----`);
+  console.log(`----一共有${msg.length}组----`);
+  let writerStream = fs.createWriteStream("photocs.json");
+  writerStream.write(JSON.stringify(msg, undefined, 2), "UTF8");
+  writerStream.end();
   for (let i = 0; i < msg.length; i++) {
     try {
       await page.goto("http://desk.zol.com.cn" + msg[i]);
@@ -61,18 +82,18 @@ const down = async () => {
       await page.goto(hrefItems);
       await page.waitForSelector("body img", { visible: true });
       const hrefItem = await page.$eval("body img", el => el.src);
-      sleep(1000);
       console.log(`------下载地址：${hrefItem}`);
       let out = fs.createWriteStream(
-        path.resolve(__dirname + "/photosall", `${hrefTitle}_${i}_.jpg`),
+        path.resolve(__dirname + "/photosalls", `${hrefTitle}_${i}_.jpg`),
         {
           autoClose: true,
         }
       );
+      Sleep(1000);
       rp(opts({ url: hrefItem })).pipe(out);
       out.on("error", err => {
         error.push(0);
-        console.log(`错误原因：${err.stack} 共出错${error.length}张`);
+        console.log(`错误照片：${hrefItem} 共出错${error.length}张`);
       });
     } catch (error) {
       console.log("error: " + error);
@@ -103,6 +124,7 @@ const down = async () => {
   // });
 
   console.log(`----over----${new Date()}`);
+  // await page.waitFor(3000);
   browser.close();
 };
 down();
